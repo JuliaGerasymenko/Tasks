7628ded34f24f0c045a104463e19a86e1c799aff
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-07 13:21:20 +0200

    fixup #3

--- task1.js
+++ task1.js
@@ -1,48 +1,48 @@
 'use strict';
 
-let base = 1000000;
+let base = 10000;
+let baseLog = Math.log10(10*base);
 
 function addition(arr1, arr2) {
-  if (arr1.length < arr2.length) {
-    [arr1, arr2] = [arr2, arr1];
-  }
+  if (arr1.length < arr2.length) [arr1, arr2] = [arr2, arr1];
   if (arr2[0] === 0 && arr2.length === 1) return arr1;
+
   let len1 = arr1.length;
   let len2 = arr2.length;
+  let res = new Array(len1).fill(0);
 
-  for (let i = 0; i < len2; i++) {
-    arr1[i] += arr2[i];
-    if (arr1[i] >= base) {
-      arr1[i] -= base;
-      i === len1 - 1 ? arr1.push(1) : (arr1[i + 1] += 1);
+  for (let i = 0; i < len1; i++) {
+    res[i] += arr1[i] + (arr2[i] === undefined ? 0 : arr2[i]);
+    if (res[i] >= base) {
+      res[i] -= base;
+      i === len1 - 1 ? res.push(1) : (res[i + 1] += 1);
     }
   }
-  return arr1;
+  return res;
 }
 
 function multiplication(arr1, arr2) {
   if (!arr1[0] && arr1.length === 1) return arr1;
+
   let len1 = arr1.length;
   let len2 = arr2.length;
 
-  if (len1 !== len2) {
-    if (len1 < len2) {
-      while (arr2.length !== len1) {
-        arr1.push(0);
-      }
-    } else {
-      while (arr2.length !== len1) {
-        arr2.push(0);
-      }
-    }
-  }
+  // if (len1 !== len2)
+  //   if (len1 < len2) {
+  //     while (arr2.length !== len1) {
+  //       arr1.push(0);
+  //     }
+  //   } else {
+  //     while (arr2.length !== len1) arr2.push(0);
+  //   }
 
   let res = new Array(len2 + len1 - 1).fill(0);
   let resLen = res.length - 1;
+
   for (let i = 0; i < len1; i++) {
     for (let j = 0; j < len1; j++) {
       res[j + i] += arr2[i] * arr1[j];
-      if (res[j + i] > base - 1) {
+      if (res[j + i] >= base) {
         let divider = Math.floor(res[j + i] / base);
         res[j + i] %= base;
         res[j + i + 1] === undefined
@@ -52,32 +52,27 @@ function multiplication(arr1, arr2) {
     }
   }
 
-  while (!res[res.length - 1]) {
-    res.pop();
-  }
+  // while (!res[res.length - 1]) {
+  //   res.pop();
+  // }
+
   return res;
 }
 
 function fibonacci(t1, t2, n) {
   let a1 = [t1];
   let a2 = [t2];
-  let count = 3;
 
   for (let i = 3; i <= n; i++) {
     a1 = addition(a1, multiplication(a2,a2));
-    if (i !== n) {
-      let tmp = a1;
-      a1 = a2;
-      a2 = tmp;
-    }
+    if (i !== n) [a1, a2] = [a2, a1];
   }
 
   return (a1.length > 1
     ? a1.map((el, i) => {
         el = el.toString();
-        while (el.length < 6 && i !== a1.length - 1) {
-          el = "0" + el;
-        }
+        if (i === a1.length - 1) return el;
+        while (el.length <= baseLog) el = '0' + el;
         return el;
       })
     : a1
@@ -87,30 +82,3 @@ function fibonacci(t1, t2, n) {
 }
 
 console.log(fibonacci(0,1,10));
-
-// the second more slowly variant
-
-// function fibonacci(t1, t2, n) {
-//   let a1 = [t1];
-//   let a2 = [t2];
-//   let count = 3;
-  // let k;
-  // function sum(a1, a2, count) {
-  //   if (count > n) return k;
-  //   k = addition(a1, multiplication(a2, a2));
-  //   return sum(a2, k, ++count);
-  // }
-  // let sum1 = sum(a1, a2, 3);
-  // return (sum1.length > 1
-    //   ? sum1.map((el, i) => {
-      //       el = el.toString();
-      //       while (el.length < 6 && i !== sum1.length - 1) {
-        //         el = '0' + el;
-        //       }
-        //       return el;
-        //     })
-        //   : sum1
-        // )
-        //   .reverse()
-        //   .join('');
-// }
