d0ab26e45ee8b34eeb6f375149bc4397aa447de0
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-07 13:35:01 +0200

    fixup

--- seive.js
+++ seive.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let primes = [];
 
 function seiveOriginal(n) {
+  let primes = [];
   let nPrime = new Array(n + 1).fill(false);
   nPrime[0] = true;
   nPrime[1] = true;
@@ -14,13 +14,12 @@ function seiveOriginal(n) {
       }
     }
   }
-  return nPrime;
+  return primes;
 }
 
 function blocksSeive(n) {
   const sqrt = Math.sqrt(n);
-  seiveOriginal(sqrt);
-
+  const primes = seiveOriginal(sqrt);
   const s = 6;
   let result = 0;
   let nonPrime = new Array(s);
@@ -30,22 +29,21 @@ function blocksSeive(n) {
     for (let i = 0; i < primes.length; i++) {
       let start_id = Math.floor((s * k - 1) / primes[i]) + 1;
       let j = Math.max(start_id, 2) * primes[i] - k * s;
-      for (; j < s;j += primes[i]) {
-        nonPrime[j] = true;
-      }
+      for (; j < s;j += primes[i]) nonPrime[j] = true;
     }
 
     if (!k) {
       nonPrime[0] = true;
       nonPrime[1] = true;
     }
+
     nonPrime.forEach((el, i) => {
       if (!el && k * s + i <= n) {
         result += 1;
       }
     });
   }
-  console.log(result);
+  return result;
 }
 
 seiveOriginal(36);
