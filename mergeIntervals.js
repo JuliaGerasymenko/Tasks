7d5ca1d06c4bb36b96b0b9c4ce6d9ab0da6ce8e2
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-09 21:16:15 +0200

    fix

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -1,37 +1,62 @@
 'use strict';
 
-const merge = intervals => {
-  let len = intervals.length;
-  if (len <= 1) return intervals;
-  let res = [];
-  for (let i = 0; i < intervals.length; i++) {
-    for (let j = i + 1; j < intervals.length; j++) {
-      if (intervals[i][0] > intervals[j][0]) {
-        [intervals[i], intervals[j]] = [intervals[j], intervals[i]];
-      }
+function merge(arr, start, m, end) {
+  let newArr = [];
+  let i = start,
+    j = m + 1;
+
+  while(i <= m && j <= end) {
+    if (arr[i][0] < arr[j][0]) {
+      newArr.push(arr[i]);
+      i++;
+    } else {
+      newArr.push(arr[j]);
+      j++;
     }
   }
 
-  for (let i = 0; i < len - 1; i++) {
-    if (
-      intervals[i][1] >= intervals[i + 1][0] ||
-      (res.length && res[res.length - 1][1] >= intervals[i + 1][0])
-    ) {
-      if (!res.length) {
-        res.push([
-          intervals[i][0],
-          Math.max(intervals[i][1], intervals[i + 1][1])
-        ]);
-      } else {
-        res[res.length - 1][1] = Math.max(
-          res[res.length - 1][1],
-          intervals[i + 1][1]
-        );
-      }
-    } else {
-      if (!res.length) res.push(intervals[i]);
-      res.push(intervals[i + 1]);
+  while (i <= m) {
+    newArr.push(arr[i]);
+    i++;
+  }
+
+  while(j <= end) {
+    newArr.push(arr[j]);
+    j++;
+  }
+  for (let i = start; i <= end ; i++) arr[i] = newArr[i-start];
+}
+
+function mergeSort(arr, i, j) {
+  if (i < j) {
+    let m = Math.floor((i+j)/2);
+    mergeSort(arr, i, m);
+    mergeSort(arr, m+1, j);
+    merge(arr, i, m, j);
+  }
+}
+
+function concat(intervals) {
+  let len = intervals.length;
+  if (len <= 1) return intervals;
+
+  mergeSort(intervals, 0, len-1);
+
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = i + 1; j < intervals.length; j++) {
+      if (intervals[i][0] > intervals[j][0])
+        [intervals[i], intervals[j]] = [intervals[j], intervals[i]];
     }
   }
+
+  let res = [intervals.shift()];
+  for (let interval of intervals) {
+    let lastIndex = res.length - 1;
+    interval !== res[lastIndex] && interval[0] <= res[lastIndex][1]
+      ? res[lastIndex][1] = Math.max(res[lastIndex][1], interval[1])
+      : res.push(interval);
+  }
+
   return res;
 };
+console.log(concat([[1,3],[2,6],[8,10],[15,18]]));
