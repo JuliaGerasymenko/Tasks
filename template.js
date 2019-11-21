e8318ee97892d5ddbbef5d9439ee5e9daba72d96
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-21 13:04:06 +0200

    implemented a class Shared Buffer

--- template.js
+++ template.js
@@ -1,16 +1,27 @@
 'use strict';
 
-function template(str, arr = []) {
-  let index = str.indexOf('*');
+class StringBuffer {
+  constructor() {
+    this.buffer = [];
+    this.index = 0;
+  }
+  add(str) {
+    this.buffer[this.index] = str;
+    this.index++;
+  }
+}
+
+function template(str, arr = new StringBuffer(), start = 0) {
+  let index = str.indexOf('*', start);
   if (index === -1) {
-    arr.push(str);
+    arr.add(str);
     return;
   }
   let newStr = str.substring(0, index) + '0' + str.substring(index+1);
-  template(newStr, arr);
+  template(newStr, arr, index+1);
   newStr = str.substring(0, index) + '1' + str.substring(index+1);
-  template(newStr, arr);
-  return arr;
+  template(newStr, arr, index+1);
+  return arr.buffer;
 }
 
 console.log(template('0*1*'));
