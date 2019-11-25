f037655c144e25abf3f16744507780e34c10ad54
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-25 14:56:08 +0200

    De Morgan's laws

--- validBST.js
+++ validBST.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var isValidBST = function(root, lowerBound = -Infinity, higherBound = Infinity) {
-
+var isValidBST = function(
+  root,
+  lowerBound = -Infinity,
+  higherBound = Infinity
+) {
   if (!root) return true;
 
-    if (root.val >= higherBound || root.val <= lowerBound) return false;
+  if (root.val >= higherBound || root.val <= lowerBound) return false;
 
-   return (!isValidBST(root.left, lowerBound, root.val) || !isValidBST(root.right, root.val, higherBound))
-       ? false : true;
+  return !(isValidBST(root.left, lowerBound, root.val) &&
+    isValidBST(root.right, root.val, higherBound))
+    ? false
+    : true;
 };
