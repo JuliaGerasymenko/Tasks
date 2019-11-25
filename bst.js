390352450d00cf172a3fd7a7189fcf9b11e17fd6
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-11-25 15:30:36 +0200

    replase a logical operator to a ternary one.

--- bst.js
+++ bst.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var recursion = function(root, min, max) {
-    if (!root) return true;
-    return root.val < max && root.val > min
-        ? recursion(root.left, min, root.val) && recursion(root.right, root.val, max)
-        : false;
+  if (!root) return true;
+  return root.val < max
+    ? root.val > min
+      ? recursion(root.left, min, root.val) &&
+        recursion(root.right, root.val, max)
+      : false
+    : false;
 };
 
-var isValidBST = function(root) {
-    if (!root || (!root.left && !root.right)) return true;
-    return recursion(root.left, -Infinity, root.val) && recursion(root.right, root.val, Infinity);
-};
+var isValidBST = root => recursion(root, -Infinity, Infinity);
